b43e002fdaa15c9e352db7e8c4d72147360785f8
1340490631 <1340490631@qq.com>
2019-06-27 09:07:20 +0800

    day17

---
+++ day17.php
@@ -0,0 +1,26 @@
+<?php
+
+$arr_A=[1,3,6,9]; 
+$arr_B=[2,4,5,8,20];
+Combine($arr_A,$arr_B) ;
+function Combine($arr_A,$arr_B) { 
+       $arr='';
+       foreach ($arr_B as $key => $v) {
+            $arr_A[]=$v;
+            $arr=$arr_A;     
+       }
+      $count= count($arr);
+
+      $temp= 0;
+      for($i=0;$i<$count;$i++){
+        for($j=0;$j<$count-1-$i ;$j++){
+            if($arr[$j]>$arr[$j+1]){
+                $temp = $arr[$j];
+                $arr[$j]=$arr[$j+1];
+                $arr[$j+1] = $temp;
+            }
+        }
+      }
+print_r($arr);
+}
+?>
